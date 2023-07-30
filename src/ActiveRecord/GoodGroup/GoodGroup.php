8a3752735fa573a121f53b4e2c76c672a40edcc6
ionov-e <user@user-pc>
2023-07-31 01:39:28 +0300

    GoodGroup done: DTO + Active Record

--- src/ActiveRecord/GoodGroup/GoodGroup.php
+++ src/ActiveRecord/GoodGroup/GoodGroup.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace VetmanagerApiGateway\ActiveRecord\GoodGroup;
 
 use VetmanagerApiGateway\ActiveRecord\AbstractActiveRecord;
+use VetmanagerApiGateway\ActiveRecordFactory;
 use VetmanagerApiGateway\DTO\GoodGroup\GoodGroupOnlyDto;
+use VetmanagerApiGateway\DTO\GoodGroup\GoodGroupOnlyDtoInterface;
 
 /**
  * @property-read GoodGroupOnlyDto $originalDto
@@ -23,7 +25,7 @@ use VetmanagerApiGateway\DTO\GoodGroup\GoodGroupOnlyDto;
  *     price_id: ?string
  * } $originalDataArray
  */
-class GoodGroup extends AbstractActiveRecord
+class GoodGroup extends AbstractActiveRecord implements GoodGroupOnlyDtoInterface
 {
     public static function getRouteKey(): string
     {
@@ -33,5 +35,82 @@ class GoodGroup extends AbstractActiveRecord
     public static function getDtoClass(): string
     {
         return GoodGroupOnlyDto::class;
+    }
+
+    public function __construct(ActiveRecordFactory $activeRecordFactory, GoodGroupOnlyDto $modelDTO)
+    {
+        parent::__construct($activeRecordFactory, $modelDTO);
+        $this->modelDTO = $modelDTO;
+    }
+
+    /** @inheritDoc */
+    public function getId(): int
+    {
+        return $this->modelDTO->getId();
+    }
+
+    public function getTitle(): string
+    {
+        return $this->modelDTO->getTitle();
+    }
+
+    /** @inheritDoc */
+    public function getIsService(): bool
+    {
+        return $this->modelDTO->getIsService();
+    }
+
+    /** @inheritDoc */
+    public function getMarkup(): ?float
+    {
+        return $this->modelDTO->getMarkup();
+    }
+
+    /** @inheritDoc */
+    public function getIsShowInVaccines(): bool
+    {
+        return $this->modelDTO->getIsShowInVaccines();
+    }
+
+    /** @inheritDoc */
+    public function getPriceId(): ?int
+    {
+        return $this->modelDTO->getPriceId();
+    }
+
+    /** @inheritDoc */
+    public function setId(?int $value): static
+    {
+        return self::setNewModelDtoFluently($this, $this->modelDTO->setId($value));
+    }
+
+    /** @inheritDoc */
+    public function setTitle(?string $value): static
+    {
+        return self::setNewModelDtoFluently($this, $this->modelDTO->setTitle($value));
+    }
+
+    /** @inheritDoc */
+    public function setIsService(?bool $value): static
+    {
+        return self::setNewModelDtoFluently($this, $this->modelDTO->setIsService($value));
+    }
+
+    /** @inheritDoc */
+    public function setMarkup(?float $value): static
+    {
+        return self::setNewModelDtoFluently($this, $this->modelDTO->setMarkup($value));
+    }
+
+    /** @inheritDoc */
+    public function setIsShowInVaccines(?bool $value): static
+    {
+        return self::setNewModelDtoFluently($this, $this->modelDTO->setIsShowInVaccines($value));
+    }
+
+    /** @inheritDoc */
+    public function setPriceId(?int $value): static
+    {
+        return self::setNewModelDtoFluently($this, $this->modelDTO->setPriceId($value));
     }
 }
--- src/DTO/GoodGroup/GoodGroupOnlyDto.php
+++ src/DTO/GoodGroup/GoodGroupOnlyDto.php
@@ -5,23 +5,93 @@ declare(strict_types=1);
 namespace VetmanagerApiGateway\DTO\GoodGroup;
 
 use VetmanagerApiGateway\DTO\AbstractDTO;
-use VetmanagerApiGateway\Exception\VetmanagerApiGatewayException;
+use VetmanagerApiGateway\Exception\VetmanagerApiGatewayInnerException;
+use VetmanagerApiGateway\Exception\VetmanagerApiGatewayResponseException;
 use VetmanagerApiGateway\Hydrator\ApiBool;
 use VetmanagerApiGateway\Hydrator\ApiFloat;
 use VetmanagerApiGateway\Hydrator\ApiInt;
 use VetmanagerApiGateway\Hydrator\ApiString;
 
-final class GoodGroupOnlyDto extends AbstractDTO
+class GoodGroupOnlyDto extends AbstractDTO implements GoodGroupOnlyDtoInterface
 {
-    /** @var positive-int */
-    public int $id;
-    public string $title;
-    public ?int $priceId;
-    /** Default: 0 */
-    public bool $isService;
-    public ?float $markup;
-    /** Default: 0 */
-    public bool $isShowInVaccines;
+    /**
+     * @param string|null $id
+     * @param string|null $title
+     * @param string|null $is_service Default: 0
+     * @param string|null $markup
+     * @param string|null $is_show_in_vaccines Default: 0
+     * @param string|null $price_id
+     */
+    public function __construct(
+        protected ?string $id,
+        protected ?string $title,
+        protected ?string $is_service,
+        protected ?string $markup,
+        protected ?string $is_show_in_vaccines,
+        protected ?string $price_id
+    ) {
+    }
+
+    public function getId(): int
+    {
+        return ApiInt::fromStringOrNull($this->id)->getPositiveInt();
+    }
+
+    public function getTitle(): string
+    {
+        return ApiString::fromStringOrNull($this->title)->getStringEvenIfNullGiven();
+    }
+
+    public function getIsService(): bool
+    {
+        return ApiBool::fromStringOrNull($this->is_service)->getBoolOrThrowIfNull();
+    }
+
+    public function getMarkup(): ?float
+    {
+        return ApiFloat::fromStringOrNull($this->markup)->getNonZeroFloatOrNull();
+    }
+
+
+    public function getIsShowInVaccines(): bool
+    {
+        return ApiBool::fromStringOrNull($this->is_show_in_vaccines)->getBoolOrThrowIfNull();
+    }
+
+    public function getPriceId(): ?int
+    {
+        return ApiInt::fromStringOrNull($this->price_id)->getPositiveIntOrNull();
+    }
+
+    public function setId(?int $value): static
+    {
+        return self::setPropertyFluently($this, 'id', is_null($value) ? null : (string)$value);
+    }
+
+    public function setTitle(?string $value): static
+    {
+        return self::setPropertyFluently($this, 'title', $value);
+    }
+
+    public function setIsService(?bool $value): static
+    {
+        return self::setPropertyFluently($this, 'is_service', is_null($value) ? null : (string)(int)$value);
+    }
+
+    public function setMarkup(?float $value): static
+    {
+        return self::setPropertyFluently($this, 'markup', is_null($value) ? null : (string)$value);
+    }
+
+    public function setIsShowInVaccines(?bool $value): static
+    {
+        return self::setPropertyFluently($this, 'is_show_in_vaccines', is_null($value) ? null : (string)(int)$value);
+    }
+
+    public function setPriceId(?int $value): static
+    {
+        return self::setPropertyFluently($this, 'price_id', is_null($value) ? null : (string)$value);
+    }
 
     /** @param array{
      *     "id": string,
@@ -31,30 +101,5 @@ final class GoodGroupOnlyDto extends AbstractDTO
      *     "is_show_in_vaccines": string,
      *     "price_id": ?string
      * } $originalDataArray
-     * @throws VetmanagerApiGatewayException
-     * @psalm-suppress MoreSpecificImplementedParamType
      */
-    public static function fromApiResponseArray(array $originalDataArray): self
-    {
-        $instance = new self($originalDataArray);
-        $instance->id = ApiInt::fromStringOrNull($originalDataArray['id'])->getPositiveInt();
-        $instance->title = ApiString::fromStringOrNull($originalDataArray['title'])->getStringEvenIfNullGiven();
-        $instance->priceId = ApiInt::fromStringOrNull($originalDataArray['price_id'])->getPositiveIntOrNull();
-        $instance->isService = ApiBool::fromStringOrNull($originalDataArray['is_service'])->getBoolOrThrowIfNull();
-        $instance->markup = ApiFloat::fromStringOrNull($originalDataArray['markup'])->getNonZeroFloatOrNull();
-        $instance->isShowInVaccines = ApiBool::fromStringOrNull($originalDataArray['is_show_in_vaccines'])->getBoolOrThrowIfNull();
-        return $instance;
-    }
-
-    /** @inheritdoc */
-//    protected function getSetValuesWithoutId(): array
-//    {
-//        return array_merge(
-//            property_exists($this, 'title') ? ['title' => $this->title] : [],
-//            property_exists($this, 'priceId') ? ['price_id' => $this->priceId] : [],
-//            property_exists($this, 'isService') ? ['is_service' => (int)$this->isService] : [],
-//            property_exists($this, 'markup') ? ['markup' => $this->markup] : [],
-//            property_exists($this, 'isShowInVaccines') ? ['is_show_in_vaccines' => (int)$this->isShowInVaccines] : [],
-//        );
-//    }
 }
